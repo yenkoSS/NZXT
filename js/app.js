daa46acda4908e8270443c0f44e8712f08f231ca
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2024-11-06 11:39:14 +0200

    add dynamic behaviour for responsive navbar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
+const navResponsive = document.querySelector(".nav-list-responsive");
+const menuBtn = document.querySelector(".menu-icon");
+const iconClose = document.querySelector(".icon-close");
+const navListRespList = document.querySelectorAll(".nav-item");
+console.log(navListRespList);
+
+navListRespList.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    if (navResponsive.style.display == "flex") {
+      navResponsive.style.display = "none";
+    }
+  });
+});
+
+menuBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const menuDisplay = window
+    .getComputedStyle(navResponsive)
+    .getPropertyValue("display");
+
+  if (menuDisplay == "" || menuDisplay == "none") {
+    navResponsive.style.display = "flex";
+  } else if (menuDisplay == "flex") {
+    navResponsive.style.display = "none";
+  }
+});
+
+iconClose.addEventListener("click", (e) => {
+  e.preventDefault();
+  navResponsive.style.display = "none";
+});
+
 const headingObserver = new IntersectionObserver((entries) =>
   entries.forEach((entry) => {
     entry.target.classList.toggle("show", entry.isIntersecting);
